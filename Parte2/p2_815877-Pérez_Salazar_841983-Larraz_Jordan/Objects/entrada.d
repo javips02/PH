6ca7293684e54a2007422fcc48dd48faa47cb793
Javier <119667872+javips02@users.noreply.github.com>
2023-10-17 00:04:58 +0200

    17/10

---
+++ Parte2/p2_815877-Pérez_Salazar_841983-Larraz_Jordan/Objects/entrada.d
@@ -0,0 +1,4 @@
+.\objects\entrada.o: entrada.c
+.\objects\entrada.o: entrada.h
+.\objects\entrada.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\inttypes.h
+.\objects\entrada.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
